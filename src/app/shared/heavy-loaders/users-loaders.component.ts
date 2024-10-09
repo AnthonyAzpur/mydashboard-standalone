183643973c3462b50198f8258a67f901b84cc91e
Anthony <anthony12al20@gmail.com>
2024-10-09 18:08:56 -0500

    Descripción de los cambios realizados

---
+++ src/app/shared/heavy-loaders/users-loaders.component.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-loader',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<h1>hola</h1>`,
+})
+
+export class UserLoaderComponent {
+  constructor() { }
+
+  
+}
---
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { routes } from '../../app.routes';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidemenu',
+  standalone: true,
+  imports: [CommonModule , RouterModule ],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.css'
+})
+export class SidemenuComponent {
+  public menuItens = routes
+  .map( route =>route.children ?? [])
+  .flat()
+  .filter(route => !route.path?.includes(':'))
+  .filter(route => route && route.path );
+  //
+  constructor(){
+
+    // const dashboardRoutes= routes
+    // .map( route =>route.children ?? [])
+    // .flat()
+    // .filter(route => !route.path?.includes(':'))
+    // .filter(route => route && route.path )
+    //console.log(dashboardRoutes); ver las rutas
+  }
+}
